572548b07312c3d08768698ac6a3d7bb94b80d9d
Jesse Gonzalez <jessegonzalez024@gmail.com>
2019-11-10 03:27:17 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -119,8 +119,9 @@ def adding_to_dennis
         },
         :dennis_ritchie => {
           :known_for => "Unix",
-          :languages => ["C", ]
+          :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] << 
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
